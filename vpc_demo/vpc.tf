357d37c24141e8ab8706f0173594cb1e76657a6c
Thiago Hickmann <>
2020-07-29 02:25:48 +0800

    .tf.example para provisonar subnet e instância

--- vpc_demo/vpc.tf
+++ vpc_demo/vpc.tf
@@ -1,37 +1,19 @@
 resource "huaweicloud_vpc_v1" "vpc1" {
-  name = "vpc-webinar"
-  cidr = "192.168.0.0/16"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
   tags = {
-    project = "Webinar"
-    #    terraform = "awesome"
+    project   = "Webinar"
+    terraform = "awesome"
   }
 }
 
 
+variable "vpc_name" {
+  default = "vpc-webinar-automation"
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#variable "vpc_name" {
-#  default = "huaweicloud_vpc"
-#}
-
-#variable "vpc_cidr" {
-#  default = "192.168.0.0/16"
-#}
+variable "vpc_cidr" {
+  default = "192.168.0.0/16"
+}
 
